d547038cd83a5a019e55f9f8e84a0f9142df5241
Chidozie <c.ohiaeri@orbsolutions.com.ng>
2018-07-28 11:43:17 +0100

    Question 1 second commit

--- applicationform.php
+++ applicationform.php
@@ -16,9 +16,9 @@
         <input type="text" placeholder="Surname" name="surname"><br><br><br>
         <input type="text" placeholder="Phone number" name="phone"><br><br><br>
         <input type="text" placeholder="Email"  name="email"><br><br><br>
-
-        Passport Photograph <input type="file" name="passport"><br><br><br>
-        Resume <input type="file" name="resume"><br><br>
+        <textarea name="cover_letter" placeholder="Cover Letter"></textarea><br><br>
+       <!--  Passport Photograph <input type="file" name="passport"><br><br><br>
+        Resume <input type="file" name="resume"><br><br> -->
 
         <input type="submit" value="Apply">
     </form>
--- apply.php
+++ apply.php
@@ -1,10 +1,39 @@
 <?php
- $_POST['first_name'] = $first_name;
- $_POST['surname'] = $surname;
- $_POST['phone'] = $phone;
- $_POST['email'] = $email;
- $_POST['passport'] = $passport;
- $_POST['resume'] = $resume;
+ $first_name = $_POST['first_name'];
+ $surname = $_POST['surname'];
+ $phone = $_POST['phone'];
+ $email = $_POST['email'];
+ $cover_letter = $_POST['cover_letter'];
+ //$passport = $_POST['passport'];
+ //$resume = $_POST['resume'];
+
+
+ $conn = mysqli_connect('localhost', 'root', '', 'application');
+
+ if($conn){
+    echo 'connected <br><br><br>';
+} else {
+    echo 'not connected<br>';
+}
+
+$qry = "SELECT count(id) as cid  from appform";
+
+$res = mysqli_query($conn, $qry);
+$ddd = mysqli_fetch_array($res);
+//echo $ddd['cid'];
+
+ if ($ddd['cid'] >= 4){
+   echo 'Application closed';
+} else {
+    $sql="INSERT INTO appform VALUES (null, '$first_name', '$surname', '$phone', '$email', '$cover_letter')";
+    $result = mysqli_query($conn, $sql);
+    
+    if ($result){
+        echo 'Success';
+    } else {
+        echo 'failed';
+    }
+}
 
 
 ?>
